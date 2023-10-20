f90283ee2fb405694f838de6ab9ff64aa4dabcb7
Shamal Sundar k <137757946+shamalsundark@users.noreply.github.com>
2023-10-20 19:15:05 +0530

    Create Sorting.js

---
+++ Sorting.js
@@ -0,0 +1,4 @@
+const fruits = ['apple', 'banana', 'cherry', 'date'];
+
+fruits.sort(); // Sorts the array in alphabetical order
+console.log(fruits); // Output: ['apple', 'banana', 'cherry', 'date']
